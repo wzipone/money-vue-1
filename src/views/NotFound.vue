5592c7c68cb2ce6f071533d8f6bd51b74d84b750
wzipone <wzipone76@gmail.com>
2020-06-07 10:51:59 +0800

    实现404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>404
+<!--        <a href="#/">返回首页</a> 会有警告-->
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound.vue'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
