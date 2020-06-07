d4b96209d53691cfa0bce16888743c84b5598994
wzipone <wzipone76@gmail.com>
2020-06-07 10:09:53 +0800

    创建NAV雏形

---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>labels</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Labels.vue'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>statistics</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Statistics.vue'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
