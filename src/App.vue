9d15d8e1e380d4c20336a1c63e331b166cd78035
wzipone <wzipone76@gmail.com>
2020-06-07 10:45:07 +0800

    Nav作为全局组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <div id="nav">
-      <router-link to="/money">money</router-link> |
-      <router-link to="/labels">labels</router-link> |
-      <router-link to="/statistics">statistics</router-link>
-    </div>
 
   </div>
 </template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="nav">
+        <router-link to="/money">money</router-link> |
+        <router-link to="/labels">labels</router-link> |
+        <router-link to="/statistics">statistics</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Nav.vue'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
